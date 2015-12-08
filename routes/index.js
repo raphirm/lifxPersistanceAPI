78fa51e320568eda03024bf93822ab48dcebeeef
raphirm <raphi.rm@gmail.com>
2015-12-08 14:45:49 +0100

    json escapes too much

--- routes/index.js
+++ routes/index.js
@@ -53,6 +53,30 @@ router.get('/scene/:uuid', function(req, res, next) {
   act_req.write('');
   act_req.end();
 });
+router.get('/scene/:uuid/power', function(req, res, next) {
+  var sceneid = req.params.uuid;
+  var data = '{\"ignore\": \"power\"}';
+  var activate_scene = {
+    host: 'api.lifx.com',
+    port: '443',
+    path: '/v1/scenes/scene_id:'+sceneid+'/activate',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data),
 
+      'Authorization': 'Bearer '+config.data.token,
+    }
+  };
+  var act_req = https.request(activate_scene, function(resi) {
+    resi.setEncoding('utf8');
+    resi.on('data', function (chunk) {
+      console.log('Response: ' + chunk);
+      res.send(chunk);
+    });
+  });
+  act_req.write(data);
+  act_req.end();
+});
 
 module.exports = router;
